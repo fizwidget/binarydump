ef57304565154f7a5d4a74a8a695d7e4f9d08b7f
James Russell <fizwidget@gmail.com>
2011-11-05 22:27:39 +1030

    added more descriptive comments to start of file

--- binarydump.c
+++ binarydump.c
@@ -1,7 +1,16 @@
 /**
  * binarydump.c
  *
- * Just like hexdump, but in binary!
+ * Simple command line utility that displays file contents in binary.
+ * Example output:
+ *
+ * 0x0	01101000   01100101   01101100   01101100   
+ * 0x4	01101111   00100000   01110111   01101111   
+ * 0x8	01110010   01101100   01100100   00100000
+ *
+ * By default, it prints hexadecimal offset addresses. The '-r' option disables
+ * address printing, and removes the whitespace that is normally printed between bytes.
+ * The '-n' option controls the number of bytes printed per line of output.
  *
  * Author: James Russell (fizwidget)
  */
